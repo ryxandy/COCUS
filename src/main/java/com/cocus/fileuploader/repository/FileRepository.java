868745ba27b4b4b6b6b7110df1748f4677e05caf
ryxandy <ryxandy@hotmail.com>
2024-06-07 21:04:09 -0300

    adding basic repository

---
+++ src/main/java/com/cocus/fileuploader/repository/FileRepository.java
@@ -0,0 +1,7 @@
+package com.cocus.fileuploader.repository;
+
+import com.cocus.fileuploader.model.FileEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FileRepository extends JpaRepository<FileEntity, Long> {
+}
