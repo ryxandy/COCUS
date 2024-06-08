209895851be1250d0dabc8ce937058fd3c094f51
ryxandy <ryxandy@hotmail.com>
2024-06-07 21:00:30 -0300

    starting project, adding lombok and creating the file entity

---
+++ src/main/java/com/cocus/fileuploader/model/FileEntity.java
@@ -0,0 +1,23 @@
+package com.cocus.fileuploader.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class FileEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String fileName;
+
+    @ElementCollection
+    private List<String> lines;
+}
